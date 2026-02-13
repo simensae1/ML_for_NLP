6777af583b1c8a1bb12055b80573b648d1107913
simensae1 <tsang.simeon@gmail.com>
2026-02-13 16:51:22 +0000

    add req

---
+++ load_data.py
@@ -0,0 +1,20 @@
+import boto3
+import pandas as pd
+import io
+
+# 1. Initialize the S3 client
+s3_client = boto3.client('s3')
+
+# 2. Get the object from S3
+bucket_name = 'sim'
+file_key = '/mlfornlp/archelec_metadata.csv'
+
+response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+
+# 3. Read the body of the response (the CSV content)
+file_content = response['Body'].read()
+
+# 4. Load into Pandas using io.BytesIO
+df = pd.read_csv(io.BytesIO(file_content))
+
+print(df.head())
